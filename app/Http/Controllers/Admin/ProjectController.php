c90e8b1730213d8988d6f86571a1c16ff2b8c8ee
TomasBelfonte <tomas.belfonte@gmail.com>
2023-02-07 18:23:51 +0100

    fix prject controller

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\StoreProjectRequest;
 use App\Models\Project;
+use Illuminate\Contracts\Cache\Store;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\View\View;
 
 class ProjectController extends Controller
@@ -73,7 +75,8 @@ class ProjectController extends Controller
      */
     public function edit(Project $project)
     {
-         return view("admin.projects.edit", compact("project"));
+
+        return view("admin.projects.edit", compact("project"));
     }
 
     /**
@@ -85,7 +88,28 @@ class ProjectController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $project = Project::FindOrFail($id);
+        $data = $request->all();
+
+        if (key_exists("cover_img", $data)) {
+            // carico il nuovo file
+            // salvo in una variabile temporanea il percorso del nuovo file
+            $path = Storage::put("projects", $data["cover_img"]);
+
+            // Dopo aver caricato la nuova immagine, PRIMA di aggiornare il db,
+            // cancelliamo dallo storage il vecchio file.
+            // $project->cover_img // vecchio file
+            Storage::delete($project->cover_img);
+        }
+
+        $project->update([
+            ...$data,
+            // Se $path ha un valore, significa che abbiamo caricato un nuovo file.
+            // Altrimenti, usiamo il percorso vecchio tramite $project->cover_img
+            "cover_img" => $path ?? $project->cover_img
+        ]);
+
+        return redirect()->route("admin.project.show", compact("project"));
     }
 
     /**
@@ -96,6 +120,12 @@ class ProjectController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $project = Project::findOrFail($id);
+
+        if ($project->cover_img) {
+            Storage::delete($project->cover_img);
+        }
+
+        $project->delete();
     }
 }
